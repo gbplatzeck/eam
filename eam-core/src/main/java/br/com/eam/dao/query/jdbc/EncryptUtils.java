2538ab034a3d5ca69086091455390704f3e952ac
Guilherme Platzeck <guilherme.platzeck@ifood.com.br>
2016-04-11 02:02:42 -0300

    adding useful javadocs

--- eam-core/src/main/java/br/com/eam/dao/query/jdbc/EncryptUtils.java
+++ eam-core/src/main/java/br/com/eam/dao/query/jdbc/EncryptUtils.java
@@ -13,11 +13,16 @@ import javax.crypto.spec.SecretKeySpec;
 
 public class EncryptUtils {
 
+	/**
+	 * 
+	 * @param plainText to be encrypted
+	 * @param key for encryption with length: 16 bytes
+	 * @return encrypted text
+	 */
 	public static String encrypt(String plainText, String key) {
 		try {
 			Cipher cipher = getCipher(Cipher.ENCRYPT_MODE, key);
-			byte[] encryptedBytes;
-			encryptedBytes = cipher.doFinal(plainText.getBytes());
+			byte[] encryptedBytes = cipher.doFinal(plainText.getBytes());
 			return Base64.getEncoder().encodeToString(encryptedBytes);
 		} catch (IllegalBlockSizeException | BadPaddingException e) {
 			throw new RuntimeException(e);
@@ -25,6 +30,12 @@ public class EncryptUtils {
 
 	}
 
+	/**
+	 * 
+	 * @param encrypted text
+	 * @param  key for encryption with length: 16 bytes
+	 * @return decrypted text
+	 */
 	public static String decrypt(String encrypted, String key) {
 		try {
 			Cipher cipher = getCipher(Cipher.DECRYPT_MODE, key);
